292c5bad58a01af0fb9d0fc6792e3c8df3453a13
Ederson Machado <edersonmberti@gmail.com>
2020-08-05 11:18:07 -0300

    add dog activity and view model

--- app/src/main/java/com/example/random_dog/dog/DogActivity.kt
+++ app/src/main/java/com/example/random_dog/dog/DogActivity.kt
@@ -1,12 +1,53 @@
 package com.example.random_dog.dog
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.bumptech.glide.Glide
 import com.example.random_dog.R
+import com.example.random_dog.model.DogResponse
+import com.example.random_dog.service.RetrofitClient
+import com.example.random_dog.util.DogViewModelFactory
+import kotlinx.android.synthetic.main.activity_dog.*
 
 class DogActivity : AppCompatActivity() {
+
+    private lateinit var dogViewModel: DogViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dog)
+
+        dogViewModel = ViewModelProvider(
+            this,
+            DogViewModelFactory(RetrofitClient.getDogService())
+        ).get(DogViewModel::class.java)
+
+        initiateObservers()
+
+        dogViewModel.getRandomDog()
+
+        button.setOnClickListener {
+            dogViewModel.getRandomDog()
+        }
+    }
+
+    private fun initiateObservers() {
+        dogViewModel.run {
+            dog.observe(this@DogActivity, Observer { setDogImage(it) })
+        }
+    }
+
+    private fun setDogImage(dogResponse: DogResponse) {
+        dogResponse.run {
+            Glide.with(this@DogActivity)
+                .load(imageUrl)
+                .placeholder(R.color.cello)
+                .error(ColorDrawable(Color.RED))
+                .into(image)
+        }
     }
 }
--- app/src/main/java/com/example/random_dog/dog/DogViewModel.kt
+++ app/src/main/java/com/example/random_dog/dog/DogViewModel.kt
@@ -1,2 +1,51 @@
 package com.example.random_dog.dog
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.random_dog.model.DogResponse
+import com.example.random_dog.service.DogService
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class DogViewModel(private val dogService: DogService): ViewModel() {
+
+    private val _showLoader = MutableLiveData<Boolean>()
+    val showLoader: LiveData<Boolean>
+        get() = _showLoader
+
+    private val _showMessageError = MutableLiveData<Boolean>()
+    val showMessageError: LiveData<Boolean>
+        get() = _showMessageError
+
+    private val _dog = MutableLiveData<DogResponse>()
+    val dog: LiveData<DogResponse>
+        get() = _dog
+
+    fun getRandomDog() {
+        _showLoader.postValue(true)
+
+        dogService.getRandomDog().enqueue(object : Callback<DogResponse> {
+            override fun onResponse(
+                call: Call<DogResponse>?,
+                response: Response<DogResponse>?
+            ) {
+                _showMessageError.postValue(response?.isSuccessful?.not())
+                _showLoader.postValue(false)
+
+                response?.takeIf { it.isSuccessful }?.run {
+                    body()?.run {
+                        _dog.postValue(this)
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call<DogResponse>?, t: Throwable?) {
+                _showMessageError.postValue(true)
+                _showLoader.postValue(false)
+            }
+        })
+    }
+
+}
